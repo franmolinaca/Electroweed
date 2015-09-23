1a00a8a835a3d4cbadb9fd83011ef3d79d76e9e0
Marco Montero <marco_montero1991@hotmail.com>
2015-09-23 17:17:30 -0600

    ultima

--- Experimento3/src/Puertinho.py
+++ Experimento3/src/Puertinho.py
@@ -3,9 +3,8 @@ import time
 
 def puertis(port):
     port = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=0)
-    while True:
-	print(port.read())
-	time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-
+    for (0, 100)
+	print (port.read())
+	i =port.read()
+	
+       
